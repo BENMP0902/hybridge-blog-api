d1f6fb31bf0f17afd405bef29fcbabd479ce9944
BENMO0902 <benjamin2401128@hybridge.education>
2026-02-24 09:03:12 -0600

    feat(data): add in-memory posts data store

---
+++ posts.js
@@ -0,0 +1,17 @@
+let posts = [
+    {
+        id: 1,
+        title: 'Primer post', 
+        content:'Contenido del primer post', 
+        author:'Admin', 
+        date: new Date() },
+    {
+        id: 2, 
+        title: 'Segundo post', 
+        content: 'Contenido del segundo post', 
+        author:'Admin', 
+        date: new Date() }
+];
+
+module.exports = posts;
+
